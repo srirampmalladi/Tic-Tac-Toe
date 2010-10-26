d1d24875a51e27a0a8462f17fcc31176b8d25efb
Sriram Malladi <srirampmalladi@gmail.com>
2010-10-26 02:51:02 -0400

    Drawing the board

--- src/org/spm/ttt/Board.java
+++ src/org/spm/ttt/Board.java
@@ -1,36 +1,52 @@
 package org.spm.ttt;
 
-import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.view.View;
 
-public class Board extends TableLayout {
-    private static final int SIZE = 3;
+public class Board extends View {
+	private static final int SIZE = 3;
 
 	public Board(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.resetBoard(SIZE);
 	}
-	
-    private void resetBoard(int size) {
-        for (int row=0;row<SIZE;row++) {
-        	TableRow tableRow = new TableRow(getContext());
-        	addView(tableRow);
-        	for (int col=0;col<SIZE;col++) {
-        		TextView cell = new TextView(getContext());
-        		cell.setText(row + "" + col);
-				tableRow.addView(cell);
-        	}
-        }
-    }
 
-	
+	private void resetBoard(int size) {
+	}
+
 	@Override
-	protected void onFinishInflate() {
-		super.onFinishInflate();
-		((Activity)getContext()).getLayoutInflater().inflate(R.layout.board, this);
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		int availableSize = Math.min(getMeasuredWidth(), getMeasuredHeight());
+		float start = 0.1f * availableSize;
+		float end = 0.9f * availableSize;
+
+		Paint innerPaint = new Paint();
+		innerPaint.setColor(Color.LTGRAY);
+		innerPaint.setStrokeWidth(2);
+
+		float dist = 0.8f * availableSize / SIZE;
+		float next = dist;
+		for (int i = 1; i < SIZE; i++) {
+			canvas.drawLine(start + next, start, start + next, end, innerPaint);
+			canvas.drawLine(start, start + next, end, start + next, innerPaint);
+			next += dist;
+
+		}
+		Paint outerPaint = new Paint();
+		outerPaint.setColor(Color.WHITE);
+		outerPaint.setStrokeWidth(5);
+		outerPaint.setStyle(Style.STROKE);
+		outerPaint.setAntiAlias(true);
+
+		RectF main = new RectF(start, start, end, end);
+		canvas.drawRoundRect(main, 5, 5, outerPaint);
 	}
 }
