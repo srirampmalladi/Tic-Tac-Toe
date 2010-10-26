6029beb8258e3e7a083605011bfaa03b05c64980
Sriram Malladi <srirampmalladi@gmail.com>
2010-10-26 01:59:48 -0400

    Whitespace

--- src/org/spm/ttt/TicTacToe.java
+++ src/org/spm/ttt/TicTacToe.java
@@ -6,10 +6,9 @@ import android.os.Bundle;
 public class TicTacToe extends Activity {
 
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
